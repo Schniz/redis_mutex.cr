6df105ead3b8742b56fe167e5fb9562dd5b702e4
Gal Schlezinger <galstar@spitfire.co.il>
2017-10-23 16:08:16 +0300

    Documentation + version bump

--- src/redis_mutex.cr
+++ src/redis_mutex.cr
@@ -1,8 +1,12 @@
 require "./redis_mutex/lock"
 
+# Distributed mutex on top of Redis
 module RedisMutex
+  # Time wait until assuming deadlock and ignoring the lock
   MAX_LOCKING_TIME = 2.seconds
 
+  # Creates a new instance of `Redis::Mutex` and executes
+  # the given block with a distributed mutex on top of Redis
   def self.run(*args, **kwargs, &block)
     Lock.new(*args, **kwargs).run(&block)
   end
